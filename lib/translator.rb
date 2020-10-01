2633196fc8459fc57458d242515006e4acf538a8
akam alam <arundelfootball23@gmail.com>
2020-10-01 23:57:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,16 +5,16 @@ require 'yaml'
 # emoticons = 
 # YAML.load_library('emoticons.yml')
 
-def load_library(file_path)
-emoticons =  YAML.load_file(file_path)
-emoticons_hash = Hash.new 
-emoticons.each do |key, value|
-  emoticons_hash[key] = Hash.new 
-  emoticons_hash[key][:english] = value[0]
-  emoticons_hash[key][:japanese] = value[1]
- end
- emoticons_hash
- end
+  def load_library(file_path)
+  emoticons =  YAML.load_file(file_path)
+  emoticons_hash = Hash.new 
+  emoticons.each do |key, value|
+    emoticons_hash[key] = Hash.new 
+    emoticons_hash[key][:english] = value[0]
+    emoticons_hash[key][:japanese] = value[1]
+   end
+   emoticons_hash
+   end
 
 # def get_japanese_emoticon(file_path, emoticon)
 # emoticons_lib = load_library(file_path)
@@ -29,13 +29,11 @@ def get_english_meaning(file_path, emoticon)
   
   emoticons_lib = load_library(file_path)
   emoticons_lib.each do |word, definitions|
-  
-  
-  end 
-  if definitions[:japanese] == emoticon
-    return word 
-  end 
-  if definitions[:japanese] != emoticon
-    puts "Sorry, that emoticon was not found"
+    if definitions[:japanese] == emoticon
+      return word 
+    end 
+    if definitions[:japanese] != emoticon
+      puts "Sorry, that emoticon was not found"
+    end 
   end 
 end
