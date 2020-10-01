f0e7de246593a595f9ca7ccd5e43d522b4468e78
akam alam <arundelfootball23@gmail.com>
2020-10-01 23:53:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,7 +32,7 @@ def get_english_meaning(file_path, emoticon)
   if definitions[:japanese] == emoticon
     return word 
     if definitions[:japanese] != emoticon
-      puts 
+      puts "Sorry, that emoticon was not found"
      end 
      
     
