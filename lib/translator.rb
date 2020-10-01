9b4fc7e58f82b277d82065ffce4dace5aefe782b
akam alam <arundelfootball23@gmail.com>
2020-10-01 23:14:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_english_meaning(file_path, emoticon)
   
   emoticons_lib = load_library(file_path)
   emoticons_lib.each do |meaning, language|
-   if emoticons_lib[language][:japanese]== emoticon
+   if emoticons_lib[language][:japanese]==emoticon
      binding.pry 
      return meaning 
   end 
